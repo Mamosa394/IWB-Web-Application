8c9e206c21a8a25035e7ca0642871401002196f4
Mamosa394 <motsiemamosa@gmail.com>
2025-04-29 16:19:03 -0700

    app,jsx

--- Frontend/main-web-app/src/App.jsx
+++ Frontend/main-web-app/src/App.jsx
@@ -11,7 +11,7 @@ import IncomeStatement from "./pages/IncomeStatement";
 import SalesDashboard from "./pages/SalesDashboard";
 import HomePage from "./components/Home-page";
 import Navbar from "./components/Nav";
-import Repairs from "./pages/Repairs";
+
 const App = () => {
   return (
     <Router>
